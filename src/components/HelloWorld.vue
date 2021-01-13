00ecfc8b5be27f07236d5550ab8779e756bfe246
careerdimasprayoga <career.dimasprayoga@gmail.com>
2021-01-14 00:06:17 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,88 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <header>
+      <b-navbar type="dark" variant="transparent" id="custom-navbar">
+        <b-navbar-nav class="ml-5">
+          <b-nav-item href="#banner" class="mr-3">Home</b-nav-item>
+          <b-nav-item href="#about" class="mr-3">About Me</b-nav-item>
+          <b-nav-item href="#" class="mr-3 navActive">Services</b-nav-item>
+          <b-nav-item href="#" class="mr-3">Skills</b-nav-item>
+          <b-nav-item href="#" class="mr-3">Projects</b-nav-item>
+          <b-nav-item href="#" class="mr-3">Contact</b-nav-item>
+        </b-navbar-nav>
+      </b-navbar>
+    </header>
+    <section class="banner" id="banner">
+      <b-container>
+        <b-row class="vh-100 text-center" align-v="center">
+          <b-col xl="12">
+            <b-img src="https://picsum.photos/200/200/?image=1025" rounded="circle" alt="Circle image" class="mb-4"></b-img>
+            <h1 class="mb-4" style="color: black; font-weight: bold;">Dimas Prayoga</h1>
+            <b-button class="main-navigation-button" variant="primary">Read More</b-button>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+    <section id="about">
+      <b-container>
+        <b-row class="vh-100 text-center">
+          <b-col xl="12">
+            <h1 class="mb-4 mt-4">About Me</h1>
+            <p>Junior Fullstack Developer</p>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+window.onscroll = function () {
+  navScrollFunction()
+}
+function navScrollFunction () {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById('custom-navbar').style.padding = '10px 10px'
+    document.getElementById('custom-navbar').classList.add('navscroll')
+  } else {
+    document.getElementById('custom-navbar').style.padding = '25px 10px'
+    document.getElementById('custom-navbar').classList.remove('navscroll')
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+@import '../assets/css/fonts.css';
+* {font-family: medium;}
+
+header #custom-navbar {
+  position: fixed;
+  width: 100%;
+  z-index: 1;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.navscroll {
+  background-color: #ffffff !important;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+header .nav-item a{
+  color: black !important;
 }
-a {
-  color: #42b983;
+header .nav-item a:hover {
+  color: white !Important;
+}
+header .nav-item:hover {
+  border-radius: 5px;
+  background-color: rgb(141, 0, 141) !important;
+}
+.navActive {
+  background-color: purple;
+  border-radius: 5px;
+}
+.banner {
+  min-height: 100vh;
+  background-image: url(../assets/banner2.jpg);
+  background-size: cover;
+}
+#about {
+  background-color: #cecece;
 }
 </style>
