8e55e33dd879330ab3c75c5d0c8c193cff2ca54e
careerdimasprayoga <career.dimasprayoga@gmail.com>
2021-01-15 12:07:35 +0700

    navbar responsive

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,31 @@
 <template>
   <div>
     <header>
-      <b-navbar type="dark" variant="transparent" id="custom-navbar">
+      <b-navbar toggleable="lg" id="custom-navbar">
+        <b-navbar-brand href="#">Dimas Prayoga</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-navbar-nav>
+              <li class="menuItems">
+                <a href="">Dimas</a>
+              </li>
+              <b-nav-item href="#">Home</b-nav-item>
+              <b-nav-item href="#" class="navActive">About</b-nav-item>
+              <b-nav-item href="#">Education</b-nav-item>
+              <b-nav-item href="#">Skills</b-nav-item>
+              <b-nav-item href="#">Project</b-nav-item>
+              <b-nav-item href="#">Contact</b-nav-item>
+            </b-navbar-nav>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+      <!--  -->
+      <!-- <b-navbar type="dark" variant="transparent" id="custom-navbar">
         <b-navbar-nav class="ml-5">
           <b-nav-item href="#banner" class="mr-3">Home</b-nav-item>
           <b-nav-item href="#about" class="mr-3">About Me</b-nav-item>
@@ -10,7 +34,7 @@
           <b-nav-item href="#" class="mr-3">Projects</b-nav-item>
           <b-nav-item href="#" class="mr-3">Contact</b-nav-item>
         </b-navbar-nav>
-      </b-navbar>
+      </b-navbar> -->
     </header>
     <section class="banner" id="banner">
       <b-container>
@@ -42,10 +66,10 @@ window.onscroll = function () {
 }
 function navScrollFunction () {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById('custom-navbar').style.padding = '10px 10px'
+    document.getElementById('custom-navbar').style.padding = '10px 60px 10px 60px'
     document.getElementById('custom-navbar').classList.add('navscroll')
   } else {
-    document.getElementById('custom-navbar').style.padding = '25px 10px'
+    document.getElementById('custom-navbar').style.padding = '25px 60px 10px 60px'
     document.getElementById('custom-navbar').classList.remove('navscroll')
   }
 }
@@ -59,6 +83,7 @@ header #custom-navbar {
   position: fixed;
   width: 100%;
   z-index: 1;
+  padding: 10px 60px 10px 60px;
 }
 .navscroll {
   background-color: #ffffff !important;
@@ -70,12 +95,23 @@ header .nav-item a:hover {
   color: white !Important;
 }
 header .nav-item:hover {
-  border-radius: 5px;
+  border-radius: 10px;
   background-color: #292826 !important;
 }
+.menuItems, .menuItems a {
+  color: black;
+  text-decoration: none;
+  padding: 5px;
+}
+.menuItems, .menuItems a:hover {
+  background-color: black;
+  color: white;
+  padding: 5px;
+  border-radius: 10px;
+}
 .navActive {
-  background-color: purple;
-  border-radius: 5px;
+  background-color: #292826;
+  border-radius: 10px;
 }
 .banner {
   min-height: 100vh;
