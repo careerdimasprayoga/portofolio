ad0ffc6cd5edb51b8fbf369628376e4cd843e267
careerdimasprayoga <career.dimasprayoga@gmail.com>
2021-01-15 13:10:49 +0700

    navbar color active solving

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,15 +11,12 @@
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
             <b-navbar-nav>
-              <li class="menuItems">
-                <a href="">Dimas</a>
-              </li>
-              <b-nav-item href="#">Home</b-nav-item>
-              <b-nav-item href="#" class="navActive">About</b-nav-item>
-              <b-nav-item href="#">Education</b-nav-item>
-              <b-nav-item href="#">Skills</b-nav-item>
-              <b-nav-item href="#">Project</b-nav-item>
-              <b-nav-item href="#">Contact</b-nav-item>
+              <b-nav-item href="#banner" class="mr-3">Home</b-nav-item>
+              <b-nav-item href="#about" class="mr-3 active">About</b-nav-item>
+              <b-nav-item href="#" class="mr-3">Education</b-nav-item>
+              <b-nav-item href="#" class="mr-3">Skills</b-nav-item>
+              <b-nav-item href="#" class="mr-3">Project</b-nav-item>
+              <b-nav-item href="#" class="mr-3">Contact</b-nav-item>
             </b-navbar-nav>
           </b-navbar-nav>
         </b-collapse>
@@ -37,25 +34,40 @@
       </b-navbar> -->
     </header>
     <section class="banner" id="banner">
+      <div class="parallax">
+        <b-container>
+          <b-row class="vh-100 text-center" align-v="center">
+            <b-col xl="12">
+              <b-img src="https://picsum.photos/200/200/?image=1025" rounded="circle" alt="Circle image" class="mb-4"></b-img>
+              <h1 class="mb-4" style="color: black; font-weight: bold;">Dimas Prayoga</h1>
+              <b-button class="main-navigation-button" variant="primary">Read More</b-button>
+            </b-col>
+          </b-row>
+        </b-container>
+      </div>
+    </section>
+    <section id="about">
       <b-container>
-        <b-row class="vh-100 text-center" align-v="center">
-          <b-col xl="12">
-            <b-img src="https://picsum.photos/200/200/?image=1025" rounded="circle" alt="Circle image" class="mb-4"></b-img>
-            <h1 class="mb-4" style="color: black; font-weight: bold;">Dimas Prayoga</h1>
-            <b-button class="main-navigation-button" variant="primary">Read More</b-button>
+        <b-row class="text-center" align-v="center" style="height: 300px;">
+          <b-col xl="10" offset="1">
+            <h3 class="mb-4">About Me</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quidem amet libero, molestiae quas ea quos eos deleniti reiciendis nemo velit repellat maiores porro obcaecati vel eius, nulla inventore. Blanditiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea, amet modi quam maiores perferendis sapiente labore sint exercitationem alias illo beatae ipsa quidem officia accusamus velit animi est praesentium.</p>
           </b-col>
         </b-row>
       </b-container>
     </section>
-    <section id="about">
-      <b-container>
-        <b-row class="vh-100 text-center">
-          <b-col xl="12">
-            <h1 class="mb-4 mt-4">About Me</h1>
-            <p>Junior Fullstack Developer</p>
-          </b-col>
-        </b-row>
-      </b-container>
+    <section class="banner" id="banner">
+      <div class="parallax">
+        <b-container>
+          <b-row class="vh-100 text-center" align-v="center">
+            <b-col xl="12">
+              <b-img src="https://picsum.photos/200/200/?image=1025" rounded="circle" alt="Circle image" class="mb-4"></b-img>
+              <h1 class="mb-4" style="color: black; font-weight: bold;">Dimas Prayoga</h1>
+              <b-button class="main-navigation-button" variant="primary">Read More</b-button>
+            </b-col>
+          </b-row>
+        </b-container>
+      </div>
     </section>
   </div>
 </template>
@@ -79,6 +91,14 @@ function navScrollFunction () {
 @import '../assets/css/fonts.css';
 * {font-family: medium;}
 
+.parallax {
+  background-image: url(../assets/images/banner2.jpg);
+  height: 100%;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 header #custom-navbar {
   position: fixed;
   width: 100%;
@@ -91,34 +111,27 @@ header #custom-navbar {
 header .nav-item a{
   color: black !important;
 }
-header .nav-item a:hover {
+/* header .nav-item a:hover {
   color: white !Important;
-}
+} */
 header .nav-item:hover {
-  border-radius: 10px;
+  border-radius: 15px;
   background-color: #292826 !important;
 }
-.menuItems, .menuItems a {
-  color: black;
-  text-decoration: none;
-  padding: 5px;
-}
-.menuItems, .menuItems a:hover {
-  background-color: black;
-  color: white;
-  padding: 5px;
-  border-radius: 10px;
-}
-.navActive {
+.active {
   background-color: #292826;
-  border-radius: 10px;
+  border-radius: 15px;
+}
+.navbar-nav > .active > a {
+  color: white !important;
+}
+.nav-item > a:hover {
+  color: white !important;
 }
 .banner {
   min-height: 100vh;
-  background-image: url(../assets/images/banner2.jpg);
-  background-size: cover;
 }
 #about {
-  background-color: #cecece;
+  background-color: rgb(248, 248, 248);
 }
 </style>
